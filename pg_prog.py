605ce1a47440e485b1c3dcf97812c0d1dc853491
ponkinalisa <ponkinalisa77@gmail.com>
2025-01-12 23:10:06 +0700

    игрок научился ходить, добавила папку с изображениями

--- pg_prog.py
+++ pg_prog.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import pygame
 
 class Board:
@@ -6,10 +8,9 @@ class Board:
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
-        # значения по умолчанию
         self.left = 10
         self.top = 10
-        self.cell_size = 30
+        self.cell_size = 60
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -24,19 +25,94 @@ class Board:
                     x * self.cell_size + self.left, y * self.cell_size + self.top,
                     self.cell_size, self.cell_size), 1)
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.x = 400
+        self.y = 400
+        self.right = False
+        self.left = False
+        self.up = False
+        self.down = True
+        self.step = 0
+        self.rotate = 0
+
+    def draw(self):
+        if self.down:
+            imp = self.load_image('Idle', 'idleDown.gif')
+        elif self.right:
+            imp = self.load_image('Idle', 'idleRight.gif')
+        elif self.left:
+            imp = self.load_image('Idle', 'idleLeft.gif')
+        else:
+            imp = self.load_image('Idle', 'idleUp.gif')
+        screen.blit(imp, (self.x, self.y))
+
+
+    def load_image(self, name1, name2, colorkey=None):
+        fullname = os.path.join('images', 'player', 'knight', 'GIFs', name1, name2)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
+
+
+
+
 if __name__ == '__main__':
     pygame.init()
-    size = 500, 500
+    all_sprites = pygame.sprite.Group()
+    size = 800, 800
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Инициализация игры')
+    pygame.display.set_caption('Игра')
+    board = Board(13, 13)
+    screen.fill((50, 50, 50))
+    board.render(screen)
 
-    # поле 5 на 7
-    board = Board(10, 10)
+    player = Player()
+    val = 10
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill((0, 0, 0))
+        userInput = pygame.key.get_pressed()
+        if userInput[pygame.K_UP]:
+            player.y -= val
+            player.right = False
+            player.left = False
+            player.up = True
+            player.down = False
+        if userInput[pygame.K_LEFT]:
+            player.x -= val
+            player.right = False
+            player.left = True
+            player.up = False
+            player.down = False
+        if userInput[pygame.K_RIGHT]:
+            player.x += val
+            player.right = True
+            player.left = False
+            player.up = False
+            player.down = False
+        if userInput[pygame.K_DOWN]:
+            player.y += val
+            player.right = False
+            player.left = False
+            player.up = False
+            player.down = True
+        screen.fill((50, 50, 50))
         board.render(screen)
+        pygame.time.delay(30)
+        player.draw()
         pygame.display.flip()
