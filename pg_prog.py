e87c8f37e9ff1a911053e67f5ea00d141c2b7b62
torbanaa22 <torbanaa22@icloud.com>
2025-01-25 14:41:02 +0700

    Исправлена часть багов

--- pg_prog.py
+++ pg_prog.py
@@ -1,9 +1,6 @@
 import pygame
-from pygame.display import update
-from screeninfo import get_monitors
 import os
 import sys
-from pygame.constants import QUIT, K_ESCAPE, KEYDOWN
 from PIL import Image
 from random import randint
 
@@ -31,7 +28,6 @@ def load_image(gif_file_path):
 
 
 
-
 class Vrag(pygame.sprite.Sprite):
     def __init__(self, n):
         super().__init__(all_sprites)
@@ -44,34 +40,34 @@ class Vrag(pygame.sprite.Sprite):
 
 
         # idle
-        self.idle1 = [load_image(os.path.join('imagi', 'Idle1.png')), load_image(os.path.join('imagi', 'Idle2.png')),
-                      load_image(os.path.join('imagi', 'Idle3.png')), load_image(os.path.join('imagi', 'Idle4.png'))]
+        self.idle1 = [load_image(os.path.join('images', 'enemy',  'Idle1.png')), load_image(os.path.join('images', 'enemy',  'Idle2.png')),
+                      load_image(os.path.join('images', 'enemy',  'Idle3.png')), load_image(os.path.join('images', 'enemy',  'Idle4.png'))]
 
         # run
         self.run1 = [
-                     load_image(os.path.join('imagi', 'Run4.png')),
-                     load_image(os.path.join('imagi', 'Run5.png')),
-                     load_image(os.path.join('imagi', 'Run6.png')), load_image(os.path.join('imagi', 'Run7.png')),
-                     load_image(os.path.join('imagi', 'Run8.png')),
-                     load_image(os.path.join('imagi', 'Run9.png'))]
+                     load_image(os.path.join('images', 'enemy',  'Run4.png')),
+                     load_image(os.path.join('images', 'enemy',  'Run5.png')),
+                     load_image(os.path.join('images', 'enemy',  'Run6.png')), load_image(os.path.join('images', 'enemy',  'Run7.png')),
+                     load_image(os.path.join('images', 'enemy',  'Run8.png')),
+                     load_image(os.path.join('images', 'enemy',  'Run9.png'))]
         # atack
-        self.atack = [load_image(os.path.join('imagi', 'Attack1.png')), load_image(os.path.join('imagi', 'Attack2.png')),
-                      load_image(os.path.join('imagi', 'Attack3.png')), load_image(os.path.join('imagi', 'Attack4.png')),
-                      load_image(os.path.join('imagi', 'Attack5.png')), load_image(os.path.join('imagi', 'Attack6.png'))]
+        self.atack = [load_image(os.path.join('images', 'enemy',  'Attack1.png')), load_image(os.path.join('images', 'enemy',  'Attack2.png')),
+                      load_image(os.path.join('images', 'enemy',  'Attack3.png')), load_image(os.path.join('images', 'enemy',  'Attack4.png')),
+                      load_image(os.path.join('images', 'enemy',  'Attack5.png')), load_image(os.path.join('images', 'enemy',  'Attack6.png'))]
         #walk
-        self.walk = [load_image(os.path.join('imagi', 'Walk1.png')), load_image(os.path.join('imagi', 'Walk2.png')),
-                     load_image(os.path.join('imagi', 'Walk3.png')), load_image(os.path.join('imagi', 'Walk4.png')),
-                     load_image(os.path.join('imagi', 'Walk5.png')), load_image(os.path.join('imagi', 'Walk6.png'))]
+        self.walk = [load_image(os.path.join('images', 'enemy',  'Walk1.png')), load_image(os.path.join('images', 'enemy',  'Walk2.png')),
+                     load_image(os.path.join('images', 'enemy',  'Walk3.png')), load_image(os.path.join('images', 'enemy',  'Walk4.png')),
+                     load_image(os.path.join('images', 'enemy',  'Walk5.png')), load_image(os.path.join('images', 'enemy',  'Walk6.png'))]
         #dead
-        self.dead = [load_image(os.path.join('imagi', 'Dead1.png')), load_image(os.path.join('imagi', 'Dead2.png')),
-                     load_image(os.path.join('imagi', 'Dead3.png')), load_image(os.path.join('imagi', 'Dead4.png')),
-                     load_image(os.path.join('imagi', 'Dead5.png')), load_image(os.path.join('imagi', 'Dead6.png')),
-                     load_image(os.path.join('imagi', 'Dead7.png')), load_image(os.path.join('imagi', 'Dead8.png'))]
+        self.dead = [load_image(os.path.join('images', 'enemy',  'Dead1.png')), load_image(os.path.join('images', 'enemy',  'Dead2.png')),
+                     load_image(os.path.join('images', 'enemy',  'Dead3.png')), load_image(os.path.join('images', 'enemy',  'Dead4.png')),
+                     load_image(os.path.join('images', 'enemy',  'Dead5.png')), load_image(os.path.join('images', 'enemy',  'Dead6.png')),
+                     load_image(os.path.join('images', 'enemy',  'Dead7.png')), load_image(os.path.join('images', 'enemy',  'Dead8.png'))]
 
         self.image = self.run1[0]
         self.move = True
         self.rect = self.image[0].get_rect()
-        print(self.rect)
+
         # self.rect.center = center
         self.frame = 0  # текущий кадр
         self.last_update = pygame.time.get_ticks()
@@ -85,7 +81,7 @@ class Vrag(pygame.sprite.Sprite):
 
 
     def draw(self):
-        kord = (400, 400)
+        kord = player.get_pos()
         now = pygame.time.get_ticks()
         if self.move:
             if now - self.last_update > self.frame_rate:
@@ -115,15 +111,27 @@ class Vrag(pygame.sprite.Sprite):
                     self.koords.insert(ind1, elem1)
                     self.koords.remove(elem)
                 else:
-                    elem1 = (elem[0] - self.step, elem[1] + self.step)
+                    elem1 = (elem[0] - self.step, elem[1] - self.step)
+                    ind1 = self.koords.index(elem)
+                    self.koords.insert(ind1, elem1)
+                    self.koords.remove(elem)
+            if elem[1] == kord[1]:
+                if elem[0] < kord[0]:
+                    elem1 = (elem[0] + self.step, elem[1])
+                    ind1 = self.koords.index(elem)
+                    self.koords.insert(ind1, elem1)
+                    self.koords.remove(elem)
+                else:
+                    elem1 = (elem[0] - self.step, elem[1])
                     ind1 = self.koords.index(elem)
                     self.koords.insert(ind1, elem1)
                     self.koords.remove(elem)
 
-            elif elem[1] == kord[1] and elem[0] == kord[0]:
+            if elem[1] == kord[1] and elem[0] == kord[0]:
                 self.move = False
             elif elem[1] != kord[1] and elem[0] != kord[0]:
                 self.move = True
+            print(elem)
         if not self.move:
             self.image = self.atack[0]
             if now - self.last_update > self.frame_rate:
@@ -132,11 +140,8 @@ class Vrag(pygame.sprite.Sprite):
                 if self.frame == len(self.atack) - 1:
                     self.frame = 0
                 self.image = self.atack[self.frame]
-
-
-
-
-
+        print(self.move)
+        print(kord)
 
 
 
@@ -158,7 +163,7 @@ class Board:
         self.cell_size = cell_size
 
     def render(self, screen):
-        id = load_image(os.path.join('imagi', 'grass.png'))
+        id = load_image(os.path.join('images', 'enemy',  'grass.png'))
         for y in range(self.height):
             for x in range(self.width):
                 pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
@@ -168,22 +173,127 @@ class Board:
                     screen.blit(id[0], (
                         x * self.cell_size + self.left, y * self.cell_size + self.top,
                         self.cell_size, self.cell_size))
-        pygame.draw.rect(screen, pygame.Color(255, 255, 255), (400, 400, 60, 60), 1)
+
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.x = 375
+        self.y = 375
+        self.right = False
+        self.left = False
+        self.up = False
+        self.down = True
+        self.step = 0
+        self.rotate = 0
+        self.move = False
+
+        self.health = 300
+        self.attack = False
+
+        #idle
+        self.idle_down = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'Idle', 'idleDown.gif'))
+        self.idle_up = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'Idle', 'idleUp.gif'))
+        self.idle_right = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'Idle', 'idleRight.gif'))
+        self.idle_left = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'Idle', 'idleLeft.gif'))
+        #run
+        self.run_down = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'run', 'runDown.gif'))
+        self.run_up = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'run', 'runUp.gif'))
+        self.run_right = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'run', 'runRight.gif'))
+        self.run_left = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'run', 'runLeft.gif'))
+        #attack
+        self.attack_down = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'attack1', 'attack1Down.gif'))
+        self.attack_up= split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'attack1', 'attack1Up.gif'))
+        self.attack_right = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'attack1', 'attack1Right.gif'))
+        self.attack_left = split_animated_gif(os.path.join('images', 'player', 'knight', 'GIFs', 'attack1', 'attack1Left.gif'))
+
+
+    def draw(self):
+        global last_update, animation_cooldown
+        imp = None
+        cur_time = pygame.time.get_ticks()
+        if self.step >= 3:
+            self.step = 0
+        if cur_time - last_update >= animation_cooldown:
+            self.step += 1
+            last_update = cur_time
+        if self.down and self.attack:
+            imp = self.attack_down[self.step]
+            self.attack = False
+        elif self.up and self.attack:
+            imp = self.attack_up[self.step]
+            self.attack = False
+        elif self.right and self.attack:
+            imp = self.attack_right[self.step]
+            self.attack = False
+        elif self.left and self.attack:
+            imp = self.attack_left[self.step]
+            self.attack = False
+        elif self.down and self.move is False:
+            imp = self.idle_down[self.step]
+        elif self.right and self.move is False:
+            imp = self.idle_right[self.step]
+        elif self.left and self.move is False:
+            imp = self.idle_left[self.step]
+        elif self.up and self.move is False:
+            imp = self.idle_up[self.step]
+        elif self.down and self.move:
+            imp = self.run_down[self.step]
+        elif self.right and self.move:
+            imp = self.run_right[self.step]
+        elif self.left and self.move:
+            imp = self.run_left[self.step]
+        elif self.up and self.move:
+            imp = self.run_up[self.step]
+        if imp:
+            screen.blit(imp, (self.x, self.y))
+
+
+    def load_image(self, name1, name2, colorkey=None):
+        fullname = os.path.join('images', 'player', 'knight', 'GIFs', name1, name2)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
+
+    def get_pos(self):
+        return [self.x, self.y]
+
+    def get_field_pos(self):
+        return [self.x // 60, self.y // 60]
+
+def split_animated_gif(gif_file_path):
+    ret = []
+    gif = Image.open(gif_file_path)
+    for frame_index in range(gif.n_frames):
+        gif.seek(frame_index)
+        frame_rgba = gif.convert("RGBA")
+        pygame_image = pygame.image.fromstring(
+            frame_rgba.tobytes(), frame_rgba.size, frame_rgba.mode)
+        ret.append(pygame_image)
+    return ret
+
 
 
 if __name__ == '__main__':
     global size, screen
     pygame.init()
     n = 5
-    for monitor in get_monitors():
-        print(f"{monitor.width}x{monitor.height}")
-        x = monitor.width
-        y = monitor.height
     all_sprites = pygame.sprite.Group()
     enemies = pygame.sprite.Group()
     size = 800, 800
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Инициализация игры')
+
     clock = pygame.time.Clock()
     vrag = Vrag(n)
     step = 10
@@ -194,15 +304,64 @@ if __name__ == '__main__':
 
 
 
+if __name__ == '__main__':
+    pygame.init()
+    n = 5
+    all_sprites = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+    size = 800, 800
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Игра')
+    clock = pygame.time.Clock()
+    vrag = Vrag(n)
+    step = 10
+
+    board = Board(13, 13)
+    screen.fill((50, 50, 50))
+    board.render(screen)
+
+    player = Player()
+    val = 10
+    animation_cooldown = 150
+    last_update = pygame.time.get_ticks()
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        screen.fill((0, 0, 0))
+        userInput = pygame.key.get_pressed()
+        if userInput[pygame.K_UP]:
+            player.y -= val
+            player.right = False
+            player.left = False
+            player.up = True
+            player.down = False
+        if userInput[pygame.K_LEFT]:
+            player.x -= val
+            player.right = False
+            player.left = True
+            player.up = False
+            player.down = False
+        if userInput[pygame.K_RIGHT]:
+            player.x += val
+            player.right = True
+            player.left = False
+            player.up = False
+            player.down = False
+        if userInput[pygame.K_DOWN]:
+            player.y += val
+            player.right = False
+            player.left = False
+            player.up = False
+            player.down = True
+        if userInput[pygame.K_UP] or userInput[pygame.K_DOWN] or userInput[pygame.K_RIGHT] or userInput[pygame.K_LEFT]:
+            player.move = True
+        else:
+            player.move = False
+        screen.fill((50, 50, 50))
         board.render(screen)
+        player.draw()
         vrag.draw()
-
         pygame.display.flip()
         clock.tick(10)
     pygame.quit()
